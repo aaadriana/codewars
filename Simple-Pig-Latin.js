ae7fb67e8a21f1b943089e4aac43ff5ba3c11253
aaadriana <adriana@stanciu.uk>
2021-08-05 21:49:39 +0100

    Create Simple-Pig-Latin.js

---
+++ Simple-Pig-Latin.js
@@ -0,0 +1,20 @@
+/*
+Move the first letter of each word to the end of it, then add "ay" to the end of the word. Leave punctuation marks untouched.
+
+Examples
+pigIt('Pig latin is cool'); // igPay atinlay siay oolcay
+pigIt('Hello world !');     // elloHay orldway !
+*/
+
+function pigIt(str) {
+  return str
+    .split(/(\W+)/)
+    .map((el) => {
+      if (!/\w/.test(el)) return el;
+      return el.slice(1) + el[0] + "ay";
+    })
+    .join("");
+}
+
+pigIt("Pig latin is cool"); //'igPay atinlay siay oolcay')
+pigIt("This is my string"); // 'hisTay siay ymay tringsay')
