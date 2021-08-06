1fe2ef4596ba862089f753e889ef033056bb3f33
aaadriana <adriana@stanciu.uk>
2021-08-06 20:56:10 +0100

    Create Reverse-every-other-word-in-the-string.js

---
+++ Reverse-every-other-word-in-the-string.js
@@ -0,0 +1,19 @@
+/*
+Reverse every other word in a given string, then return the string. Throw away any leading or trailing whitespace, while ensuring there is exactly one space between each word. Punctuation marks should be treated as if they are a part of the word in this kata.
+*/
+function reverse(str) {
+  return str
+    .split(" ")
+    .filter((item) => item !== "")
+    .map((element, index) => {
+      if (index % 2 === 1) {
+        return element.split("").reverse().join("");
+      } else {
+        return element;
+      }
+    })
+    .join(" ");
+}
+
+reverse("Reverse this string, please!"); // "Reverse siht string, !esaelp")
+reverse("I really don't like reversing strings!"); //"I yllaer don't ekil reversing !sgnirts")
